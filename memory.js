6cd5dc4a8619675900b884767812e973060ed2ed
Farbod <farbodsamane615@gmail.com>
2025-09-05 02:59:38 +0330

    Create  memory.js

---
+++ memory.js
@@ -0,0 +1,32 @@
+// حافظه دائمی با LocalStorage
+const MEMORY_KEY = 'hasham_ai_memory';
+const KNOWLEDGE_KEY = 'hasham_ai_knowledge';
+const PERSONALITY_KEY = 'hasham_ai_personality';
+
+export function loadMemory() {
+  return JSON.parse(localStorage.getItem(MEMORY_KEY)) || [];
+}
+
+export function saveMemory(memory) {
+  localStorage.setItem(MEMORY_KEY, JSON.stringify(memory));
+}
+
+export function loadKnowledge() {
+  return JSON.parse(localStorage.getItem(KNOWLEDGE_KEY)) || {};
+}
+
+export function saveKnowledge(knowledge) {
+  localStorage.setItem(KNOWLEDGE_KEY, JSON.stringify(knowledge));
+}
+
+export function loadPersonality() {
+  return JSON.parse(localStorage.getItem(PERSONALITY_KEY)) || {
+    tone: 'دوستانه',
+    style: 'خلاق',
+    name: 'هاشم',
+  };
+}
+
+export function savePersonality(personality) {
+  localStorage.setItem(PERSONALITY_KEY, JSON.stringify(personality));
+}
